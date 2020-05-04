e76c0b32813c0dd64c3c2f37b17c7928b78ec1cc
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2020-05-04 12:48:42 +0900

    :heavy_plus_sign: アセンブラ命令の変換実装

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,35 @@ import { sampleSource } from './sample_source';
 
 type WordValue = string;
 
-(async function () {
-  console.log(sampleSource);
-  // レジスタ系
-  // PR
-  // 汎用レジスタ、指標レジスタ
-  // GR0, GR1, GR2, GR3, GR4, GR5, GR6, GR7
-  // FR: OF, SF, ZF
-  // SPはいったん無視
+enum PSEUDO_INSTRUCTION {
+  DC = 'DC',
+  DS = 'DS'
+}
 
+(async function () {
   const source: (string[])[] = sampleSource;
 
-  // TODO: keyをいったんlabelにしているが、いつかアドレスにする
-  const MEMORY: {[key: string]: WordValue} = {};
+  const MEMORY: { [key: number]: WordValue } = {};
 
+  let wordCount = 0;
+  const labelToAddrMap: { [key: string]: number } = {};
+  // アセンブラ命令の処理
+  // まずラベルの対応、DC, DSを処理する
+  source.forEach(function (line) {
+    if (line[0].length) {
+      labelToAddrMap[line[0]] = wordCount;
+    }
+    if (line[1] === PSEUDO_INSTRUCTION.DC) {
+      MEMORY[wordCount] = line[2];
+    } else if (line[1] === PSEUDO_INSTRUCTION.DS) {
+      MEMORY[wordCount] = '';
+    } else {
+      MEMORY[wordCount] = JSON.stringify(line.slice(1, 5)); // TODO: ここで2語の命令に変換したい
+    }
+    wordCount += 2; // TODO: DCで複数語文の定義を要考慮
+  });
+
+  // GRの後ろ、SPはいったん無視
   const REGISTERS: { [key: string]: WordValue } = {
     PR: '',
     GR0: '',
@@ -30,7 +45,7 @@ type WordValue = string;
 
   const INSTRUCTIONS = {
     LD(rOrR1: string, adrOrR2: string, x: string) {
-      
+
     }
   };
 })();
