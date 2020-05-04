97721bcd25f960325fee667334f6f1cb42484bb2
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2020-05-04 12:27:15 +0900

    :pencil2: sample修正

--- sample/convert.js
+++ sample/convert.js
@@ -3,7 +3,7 @@ const text = fs.readFileSync(process.argv[2]);
 
 const result = [];
 text.toString().trim().replace(/\r\n?/g, "\n").split("\n").forEach(function(line) {
-	result.push([...line.split("\t"), '', '', '', ''].slice(0, 4));
+	result.push([...line.split("\t"), '', '', '', '', ''].slice(0, 5));
 });
 
 console.log(result);
