888918eb1e9f20add2d32a0ff0e07067182b7074
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2020-05-04 12:26:14 +0900

    :heavy_plus_sign: コメント追加

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,32 @@
 import { sampleSource } from './sample_source';
 
+type WordValue = string;
+
 (async function () {
   console.log(sampleSource);
+  // レジスタ系
+  // PR
+  // 汎用レジスタ、指標レジスタ
+  // GR0, GR1, GR2, GR3, GR4, GR5, GR6, GR7
+  // FR: OF, SF, ZF
+  // SPはいったん無視
+
+  const REGISTERS: { [key: string]: WordValue } = {
+    PR: '',
+    GR0: '',
+    GR1: '',
+    GR2: '',
+    GR3: '',
+    GR4: '',
+    OF: '',
+    SF: '',
+    ZF: ''
+  };
+
+  const INSTRUCTIONS = {
+    LD(rOrR1: string, adrOrR2: string, x: string) {
+
+    }
+  };
+
 })();
